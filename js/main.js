bcad59acccd11e520aa526d67a0a6d32831746cf
Daniel Camarena <dcamarena0229@gmail.com>
2019-05-05 17:45:24 -0700

    added a assets folder/ added a number generator to JS.

--- js/main.js
+++ js/main.js
@@ -22,6 +22,12 @@ document.querySelector('button').addEventListener('click',initialize)
 /*----- functions -----*/
 initialize()
 
+function generateRandNum(min,max) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
 function generateMole() {
 
 }
